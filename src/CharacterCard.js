b4c681bd61307bc585724073fda78d0d9a1c0923
Pattanun <oatinfinity42@gmail.com>
2020-09-18 21:11:22 +0700

    check3

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 export default function CharacterCard(props) {
- return (
- <div className="card">{props.value}</div>
- )
+
+
+        const [active, setActive] = useState(false);
+        const activate = () => {
+            if(!active){
+            setActive(true)
+            props.activationHandler(props.value)
+            }
+            
+           
+        setActive(true)
+        }
+        const className = `card ${active ? 'activeCard': ''}`
+        return (
+        <div className={className} onClick={activate}>{props.value}</div>
+        )
+
 }
